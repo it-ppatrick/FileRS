7f75039e4c8558a45a3b897daf3a37958bb741aa
Patrick Petrean <p.patrick@petrean.eu>
2025-07-13 18:55:14 +0200

    Initial Commit

---
+++ (01) FileRS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  (01) FileRS
+//
+//  Created by Daniel Petrean on 13.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ (01) FileRS/_01__FileRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _01__FileRSApp.swift
+//  (01) FileRS
+//
+//  Created by Daniel Petrean on 13.07.25.
+//
+
+import SwiftUI
+
+@main
+struct _01__FileRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
